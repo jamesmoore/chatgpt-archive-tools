99e85a6503dfbcd142e6838721d3070dc0bd8e2c
jamesmoore <jamesmoore@github.com>
2026-02-22 17:35:00 +0000

    extract classes to separate files

--- ChatGPTExport/Formatters/EmbeddedResourceAsset.cs
+++ ChatGPTExport/Formatters/EmbeddedResourceAsset.cs
@@ -5,15 +5,6 @@ using System.Security.Cryptography;
 
 namespace ChatGPTExport.Formatters
 {
-    public interface IFormattedConversationAsset
-    {
-        public string Name { get; }
-
-        public Stream GetStream();
-
-        public string MimeType { get; }
-    }
-
     public class EmbeddedResourceAsset(string name, string resourceName, string mimeType) : IFormattedConversationAsset
     {
         private static readonly Assembly Assembly = typeof(EmbeddedResourceAsset).Assembly;
---
+++ ChatGPTExport/Formatters/IFormattedConversationAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ChatGPTExport.Formatters
+{
+    public interface IFormattedConversationAsset
+    {
+        public string Name { get; }
+
+        public Stream GetStream();
+
+        public string MimeType { get; }
+    }
+}
