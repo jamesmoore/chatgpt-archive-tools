d85b6eb64b7a5b1c41a742eef327be8018d56ab1
jamesmoore <jamesmoore@github.com>
2026-02-25 20:36:42 +0000

    add explanation

--- ChatGpt.Archive.Api/Services/ConversationAssetsCache.cs
+++ ChatGpt.Archive.Api/Services/ConversationAssetsCache.cs
@@ -9,6 +9,7 @@ namespace ChatGpt.Archive.Api.Services
 
         public void StoreAsset(Asset asset)
         {
+            // The Skip(1) is to remove the "assets" segment from the key, which will not form part of the key built from the AssetController endpoint.
             var key = string.Join("/", asset.PathSegments.Skip(1));
             assetCache[key] = asset;
         }
