89c4ec360fb630783c48961662b2ee74f1506040
jamesmoore <jamesmoore@github.com>
2026-02-27 13:00:45 +0000

    in api asset locator, only load each directory once

--- ChatGpt.Archive.Api/Services/ApiAssetLocatorFactory.cs
+++ ChatGpt.Archive.Api/Services/ApiAssetLocatorFactory.cs
@@ -15,9 +15,12 @@ namespace ChatGpt.Archive.Api.Services
         {
             var sourceDirectories = options.SourceDirectories.Select(fileSystem.DirectoryInfo.New);
 
-            // Compared to the CLI, this uses a slightly different approach - per file here, ordered by last write time. CLI orders by parsed conversation update time (more expensive).
+            // Compared to the CLI, this uses a slightly different approach - per file here, ordered by last write time.
+            // CLI orders by parsed conversation update time (more expensive).
             var conversationFiles = conversationFinder.GetConversationFiles(sourceDirectories).SelectMany(p => p.ConversationFiles).OrderByDescending(p => p.LastWriteTimeUtc);
-            var conversationAssets = conversationFiles.Select(ConversationAssets.FromConversationsFile);
+            var grouped = conversationFiles.GroupBy(p => p.Directory?.FullName).ToList();
+            var firstFileFromEachGroup = grouped.Select(p => p.First());
+            var conversationAssets = firstFileFromEachGroup.Select(ConversationAssets.FromConversationsFile);
             var compositeAssetLocator = compositeAssetLocatorFactory.GetAssetLocator(conversationAssets);
             return new ApiAssetLocator(compositeAssetLocator, conversationAssetsCache);
         }
