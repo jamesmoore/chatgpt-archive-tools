0090f44ef9d8b831640604fdfd66157df6bdc94a
jamesmoore <jamesmoore@github.com>
2026-04-13 15:21:34 +0100

    include body in tailwind template rather than duplicate

--- ChatGPTExport/Formatters/Html/Template/TailwindHtmlFormatter.cs
+++ ChatGPTExport/Formatters/Html/Template/TailwindHtmlFormatter.cs
@@ -4,16 +4,22 @@
     {
         public string FormatHtmlPage(HtmlPage page, string pathPrefix)
         {
-            var model = new
+            var bodyModel = new
             {
                 title = page.Title,
-                headers = page.Headers,
                 body = page.Body.Select(fragment => new
                 {
                     is_user = fragment.IsUser,
                     is_writing = fragment.IsWriting,
                     html = fragment.Html,
                 }),
+            };
+
+            var model = new
+            {
+                title = page.Title,
+                headers = page.Headers,
+                body_html = TemplateRenderer.RenderTemplate("HtmlBody", bodyModel),
                 pathprefix = pathPrefix,
                 cssname = page.CssName,
             };
